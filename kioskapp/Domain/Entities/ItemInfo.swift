72a32dd686af5850e842828ac415e1293d5fdeac
tls427wodnr <tlswodnr427@naver.com>
2025-04-07 16:10:05 +0900

    feat: 모델 추가
    ItemInfo 모델, CartItem 모델, MenuItem 모델

---
+++ kioskapp/Domain/Entities/ItemInfo.swift
@@ -0,0 +1,14 @@
+//
+//  ItemInfo.swift
+//  kioskapp
+//
+//  Created by tlswo on 4/7/25.
+//
+
+import Foundation
+
+struct ItemInfo {
+    let id: UUID
+    let name: String
+    let price: Int
+}
---
+++ kioskapp/Domain/Entities/MenuItem.swift
@@ -0,0 +1,11 @@
+//
+//  MenuItem.swift
+//  kioskapp
+//
+//  Created by tlswo on 4/7/25.
+//
+
+struct MenuItem {
+    let item: ItemInfo
+    let isNew: Bool
+}

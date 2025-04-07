de9bb2a955dee18f3c398017f7e44e357b3fdc55
tls427wodnr <tlswodnr427@naver.com>
2025-04-07 16:58:10 +0900

    feat: MenuRepository 추가

--- kioskapp/Domain/Repositories/CartRepository.swift
+++ kioskapp/Domain/Repositories/CartRepository.swift
@@ -5,11 +5,13 @@
 //  Created by tlswo on 4/7/25.
 //
 
+import Foundation
+
 protocol CartRepositoryProtocol {
     func getCartItems() -> [CartItem]
     func addCartItem(_ cartItem: CartItem)
     func updateCartItem(_ cartItem: CartItem)
-    func deleteCartItem(_ cartItem: CartItem)
+    func deleteCartItem(_ id: UUID)
     func removeAllCartItems()
 }
 
@@ -30,8 +32,8 @@ class CartRepository: CartRepositoryProtocol {
         }
     }
     
-    func deleteCartItem(_ cartItem: CartItem) {
-        if let index = cartItems.firstIndex(where: { $0.item.id == cartItem.item.id }) {
+    func deleteCartItem(_ id: UUID) {
+        if let index = cartItems.firstIndex(where: { $0.item.id == id }) {
             cartItems.remove(at: index)
         }
     }

6d5ad55f5ade184741fb926f2579431085919b57
ChoiAnYong <dksdyd78@naver.com>
2025-04-09 13:39:14 +0900

    feat: HeaderView, FooterView 연결

--- kioskapp/Presentation/View/KioskMainViewController.swift
+++ kioskapp/Presentation/View/KioskMainViewController.swift
@@ -13,6 +13,9 @@ class KioskMainController: UIViewController {
         ),
         menuCartView: menuCartView
     )
+    private lazy var headerView = HeaderView()
+    private lazy var footerView = FooterView()
+    private let scrollView = UIScrollView()
     
     private let contentStack = UIStackView()
     
@@ -21,39 +24,57 @@ class KioskMainController: UIViewController {
         
         setStyle()
         setUI()
-        setLayout()
+        setLayout()        
     }
     
     private func setStyle() {
-        
         contentStack.do {
             $0.axis = .vertical
+            $0.spacing = 10
+        }
+
+        scrollView.do {
+            $0.showsVerticalScrollIndicator = false
+            $0.showsHorizontalScrollIndicator = false
+            $0.alwaysBounceVertical = true
         }
     }
     
     private func setUI() {
         view.backgroundColor = .white
-                
-        view.addSubview(contentStack)
-        
-        [menuView, menuCartView].forEach {
+        view.addSubview(headerView)
+                        
+        [menuView, menuCartView, footerView].forEach {
             contentStack.addArrangedSubview($0)
         }
+        
+        scrollView.addSubview(contentStack)
+        view.addSubview(scrollView)
     }
     
     private func setLayout() {
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(2)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalToSuperview()
+        }
         
         contentStack.snp.makeConstraints {
-            $0.edges.equalTo(view.safeAreaLayoutGuide)
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.frameLayoutGuide)
         }
         
         menuView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview()
-            $0.bottom.equalTo(menuCartView.snp.top)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(400)
         }
         
         menuCartView.snp.makeConstraints {
-            $0.top.equalTo(menuView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(250)
         }

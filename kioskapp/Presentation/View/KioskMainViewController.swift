795285057228158001a8117bbad27f454a9245de
ChoiAnYong <dksdyd78@naver.com>
2025-04-09 17:06:49 +0900

    feat: alert 및 카테고리 로직 추가

--- kioskapp/Presentation/View/KioskMainViewController.swift
+++ kioskapp/Presentation/View/KioskMainViewController.swift
@@ -12,15 +12,18 @@ class KioskMainController: UIViewController {
         viewModel: viewModel,
         menuItems: viewModel.getMenuItems().menu[0].items
     )
-    private lazy var headerView = HeaderView()
-    private lazy var footerView = FooterView()
+    private lazy var headerView = HeaderView(viewModel: viewModel)
+    private lazy var footerView = FooterView(viewModel: viewModel)
     private let scrollView = UIScrollView()
     private lazy var cartView = CartView(viewModel: viewModel)
     
     private let contentStack = UIStackView()
     
+    private var alert: UIAlertController? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        footerView.delegate = self
         bindViewModel()
         setStyle()
         setUI()
@@ -31,6 +34,12 @@ class KioskMainController: UIViewController {
         viewModel.onCartItemsUpdated = { [weak self] _ in
             guard let self else { return }
             self.cartView.reloadCart()
+            self.footerView.reloadAmount()
+        }
+        
+        viewModel.onCategoryUPdated = { [weak self] _ in
+            guard let self else { return }
+            self.menuView.reloadMenu()
         }
     }
     
@@ -49,10 +58,10 @@ class KioskMainController: UIViewController {
     
     private func setUI() {
         view.backgroundColor = .white
-                
+        view.addSubview(headerView)
         view.addSubview(contentStack)
         
-        [menuView, cartView].forEach {
+        [menuView, cartView, footerView].forEach {
             contentStack.addArrangedSubview($0)
         }
         
@@ -83,9 +92,42 @@ class KioskMainController: UIViewController {
         }
         
         cartView.snp.makeConstraints {
-            $0.top.equalTo(menuView.snp.bottom)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(250)
         }
+    }
+}
+
+extension KioskMainController {
+    private func showAlert() {
+        guard let alert else { return }
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { _ in
+            self.viewModel.removeAllCartItems()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension KioskMainController: FooterViewDelegate {
+    func didTapPaymentButton() {
+        if !viewModel.getCartItems().isEmpty {
+            alert = UIAlertController(
+                title: "성공",
+                message: "상품 주문이 완료되었습니다.",
+                preferredStyle: .alert
+            )
+        } else {
+            alert = UIAlertController(
+                title: "실패",
+                message: "장바구니가 비어있습니다.",
+                preferredStyle: .alert
+            )
+        }
+        
+        showAlert()
+    }
+    
+    func didTapCancelButton() {
+        viewModel.removeAllCartItems()
     }
 }
